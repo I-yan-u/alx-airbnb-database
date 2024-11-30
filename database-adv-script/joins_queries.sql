77458c0c5438c6f82adb36376ec814104c6e37f9
I-yan-u <iyanuajimobi12@gmail.com>
2024-11-30 08:06:31 +0100

    Refactor SQL join queries for improved readability and consistency

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,10 +1,10 @@
--- -- Inner join
--- SELECT booking.booking_id, user.user_id FROM booking
--- INNER JOIN user ON booking.user_id = user.user_id;
+-- Inner join
+SELECT booking.booking_id, user.user_id FROM booking
+INNER JOIN user ON booking.user_id = user.user_id;
 
--- -- Left join
--- SELECT property.property_id, review.rating FROM property
--- LEFT JOIN review ON property.property_id = review.property_id;
+-- Left join
+SELECT property.property_id, review.rating FROM property
+LEFT JOIN review ON property.property_id = review.property_id;
 
 -- Full outer join
 SELECT * FROM user
