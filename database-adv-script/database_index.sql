175f838069d9efffcad459ca8fbb7c635d6108f8
I-yan-u <iyanuajimobi12@gmail.com>
2024-12-01 23:30:43 +0100

    Add booking created_at index and performance analysis query for user bookings

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -4,3 +4,5 @@ CREATE INDEX idx_property_ids ON property(property_id, host_id);
 CREATE INDEX idx_property_created_at ON property(created_at);
 CREATE INDEX idx_booking_ids ON booking(booking_id, property_id, user_id);
 CREATE INDEX idx_booking_created_at ON booking(created_at);
+
+EXPLAIN ANALYZE SELECT * FROM booking WHERE user_id = '0003' AND start_date > '2024-01-01';
